16b91b91143ee3f56864677c44831cacd157ad47
williamwinter2008-prog <williamwinter2008@gmail.com>
2026-08-06 00:41:06 +0100

    Update discord-login.js

--- api/discord-login.js
+++ api/discord-login.js
@@ -3,24 +3,24 @@ export default function handler(req, res) {
     const clientID = "1532870250482237530";
 
     const redirect =
-        "https://pixelbot-dashboard-v1-23-3q9f.vercel.app/api/discord-login";
+    "https://pixelbot-dashboard-v1-23-3q9f.vercel.app/api/discord-login";
+
 
     if (!req.query.code) {
 
         const discordURL =
-            "https://discord.com/oauth2/authorize" +
-            "?client_id=" + clientID +
-            "&response_type=code" +
-            "&redirect_uri=" + encodeURIComponent(redirect) +
-            "&scope=identify%20guilds";
+        "https://discord.com/oauth2/authorize" +
+        "?client_id=" + clientID +
+        "&response_type=code" +
+        "&redirect_uri=" + encodeURIComponent(redirect) +
+        "&scope=identify%20guilds";
+
 
         return res.redirect(discordURL);
+
     }
 
-    return res.redirect("/dashboard.html");
-}
-// Discord sent us back successfully
 
-return res.redirect("/dashboard.html");
+    return res.redirect("/dashboard.html");
 
 }
