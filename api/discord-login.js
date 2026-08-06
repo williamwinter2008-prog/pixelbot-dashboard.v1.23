5fa92eb8f0b679e2c7a121b4d09ca2225947398d
williamwinter2008-prog <williamwinter2008@gmail.com>
2026-08-06 01:05:57 +0100

    Update discord-login.js

--- api/discord-login.js
+++ api/discord-login.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import cookie from "cookie";
+
+export default async function handler(req, res) {
 
     const clientID = "1532870250482237530";
 
@@ -15,12 +17,82 @@ export default function handler(req, res) {
         "&redirect_uri=" + encodeURIComponent(redirect) +
         "&scope=identify%20guilds";
 
-
         return res.redirect(discordURL);
-
     }
 
 
-    return res.redirect("/dashboard.html");
+    const params = new URLSearchParams();
+
+    params.append(
+        "client_id",
+        clientID
+    );
+
+    params.append(
+        "client_secret",
+        process.env.DISCORD_CLIENT_SECRET
+    );
+
+    params.append(
+        "grant_type",
+        "authorization_code"
+    );
+
+    params.append(
+        "code",
+        req.query.code
+    );
+
+    params.append(
+        "redirect_uri",
+        redirect
+    );
+
+
+    const tokenResponse = await fetch(
+        "https://discord.com/api/oauth2/token",
+        {
+            method:"POST",
+            headers:{
+                "Content-Type":
+                "application/x-www-form-urlencoded"
+            },
+            body:params
+        }
+    );
+
+
+    const token = await tokenResponse.json();
+
+
+    const userResponse = await fetch(
+        "https://discord.com/api/users/@me",
+        {
+            headers:{
+                Authorization:
+                `Bearer ${token.access_token}`
+            }
+        }
+    );
+
+
+    const user = await userResponse.json();
+
+
+    res.setHeader(
+        "Set-Cookie",
+        cookie.serialize(
+            "discord_user",
+            JSON.stringify(user),
+            {
+                httpOnly:false,
+                maxAge:3600,
+                path:"/"
+            }
+        )
+    );
+
+
+    res.redirect("/dashboard.html");
 
 }
